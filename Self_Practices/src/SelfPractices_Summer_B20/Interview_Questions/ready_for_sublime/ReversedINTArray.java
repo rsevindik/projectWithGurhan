d2832a408d256cd20a7a71db9864a5d76c5ea1c4
rsevindik <rsuzem@gmail.com>
2020-12-19 22:21:14 -0500

    last updates

---
+++ Self_Practices/src/SelfPractices_Summer_B20/Interview_Questions/ready_for_sublime/ReversedINTArray.java
@@ -0,0 +1,21 @@
+package SelfPractices_Summer_B20.Interview_Questions.ready_for_sublime;
+
+import java.util.Arrays;
+
+public class ReversedINTArray {
+        public static void main(String... args) {
+            int[] array = {2, 3, 45, 2, 1, 4, 5, 3};
+            System.out.println("array is: " + Arrays.toString(array));
+            reverse(array);
+            System.out.println("reverse array is: " + Arrays.toString(array));
+        }
+        private static void reverse(int[] array) {
+            int maxIndex = array.length - 1;
+            int halflength = array.length / 2;
+            for (int i = 0; i < halflength; i++) {
+                int temp = array[i];
+                array[i] = array[maxIndex - i];
+                array[maxIndex - i] = temp;
+            }
+        }
+}
